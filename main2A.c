1ff8eefe02f5959e1c27bf9ec0a99110728facd9
Volodymyr Tokar <160426346+VolodVas@users.noreply.github.com>
2024-05-15 18:24:52 +0300

    Add files via upload

--- main2A.c
+++ main2A.c
@@ -3,63 +3,82 @@
 #include <limits.h>
 
 int main() {
-    int n;
+    int choice; // Змінна для зберігання вибору користувача
 
-    // Введення кількості елементів масиву з перевіркою
-    while (1) {
-        printf("Введіть кількість елементів масиву: ");
-        if (scanf("%d", &n) == 1 && n > 0) {
-            break;
-        } else {
-            printf("Некоректне значення. Будь ласка, введіть позитивне ціле число.\n");
-            while (getchar() != '\n');  // Очищення буфера
-        }
-    }
+    do {
+        int n;
 
-    int arr[n];
-
-    // Введення елементів масиву з перевіркою
-    printf("Введіть елементи масиву:\n");
-    for (int i = 0; i < n; i++) {
+        // Введення кількості елементів масиву з перевіркою
         while (1) {
-            if (scanf("%d", &arr[i]) == 1) {  // Перевірка коректного введення
+            printf("Введіть кількість елементів масиву: ");
+            if (scanf("%d", &n) == 1 && n > 0) {
                 break;
             } else {
-                printf("Некоректне значення. Будь ласка, введіть ціле число.\n");
-                while (getchar() != '\n');  // Очищаємо буфер
+                printf("Некоректне значення. Будь ласка, введіть позитивне ціле число.\n");
+                while (getchar() != '\n');  // Очищення буфера
             }
         }
-    }
 
-    // Знаходимо максимальний за модулем елемент
-    int max_abs = INT_MIN;
-    for (int i = 0; i < n; i++) {
-        int abs_value = abs(arr[i]);
-        if (abs_value > max_abs) {
-            max_abs = abs_value;
+        int arr[n];
+
+        // Введення елементів масиву з перевіркою
+        printf("Введіть елементи масиву:\n");
+        for (int i = 0; i < n; i++) {
+            while (1) {
+                if (scanf("%d", &arr[i]) == 1) {  // Перевірка коректного введення
+                    break;
+                } else {
+                    printf("Некоректне значення. Будь ласка, введіть ціле число.\n");
+                    while (getchar() != '\n');  // Очищаємо буфер
+                }
+            }
         }
-    }
 
-    // Знаходимо індекс останнього нульового елемента
-    int last_zero_index = -1;
-    for (int i = n - 1; i >= 0; i--) {
-        if (arr[i] == 0) {
-            last_zero_index = i;
-            break;
+        // Знаходимо максимальний за модулем елемент
+        int max_abs = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            int abs_value = abs(arr[i]);
+            if (abs_value > max_abs) {
+                max_abs = abs_value;
+            }
         }
-    }
 
-    // Сума елементів після останнього нульового
-    int sum_after_last_zero = 0;
-    if (last_zero_index != -1) {
-        for (int i = last_zero_index + 1; i < n; i++) {
-            sum_after_last_zero += arr[i];
+        // Знаходимо індекс останнього нульового елемента
+        int last_zero_index = -1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] == 0) {
+                last_zero_index = i;
+                break;
+            }
         }
-    }
 
-    // Вивід результатів
-    printf("Максимальний за модулем елемент: %d\n", max_abs);
-    printf("Сума елементів після останнього нульового: %d\n", sum_after_last_zero);
+        // Сума елементів після останнього нульового
+        int sum_after_last_zero = 0;
+        if (last_zero_index != -1) {
+            for (int i = last_zero_index + 1; i < n; i++) {
+                sum_after_last_zero += arr[i];
+            }
+        } else {
+            printf("Немає жодного нульового значення в елементах масиву.\n");
+        }
+
+        // Вивід результатів
+        printf("Максимальний за модулем елемент: %d\n", max_abs);
+        if (last_zero_index != -1) {
+            printf("Сума елементів після останнього нульового: %d\n", sum_after_last_zero);
+        }
+
+        // Виведення меню
+        printf("Запустити ще раз? (1 - так, 0 - вийти): ");
+        while (1) {
+            if (scanf("%d", &choice) == 1 && (choice == 0 || choice == 1)) {
+                break;
+            } else {
+                printf("Некоректний вибір. Введіть 1 або 0: ");
+                while (getchar() != '\n');  // Очищення буфера
+            }
+        }
+    } while (choice != 0);  // Продовжуємо виконання, поки користувач не вибере 0
 
     return 0;
 }
--- main2B.c
+++ main2B.c
@@ -4,55 +4,79 @@
 #include <limits.h>
 
 int main() {
-    int n;
+    int choice; // Змінна для зберігання вибору користувача
 
-    // Введення кількості елементів масиву з перевіркою
-    while (1) {
-        printf("Введіть кількість елементів масиву: ");
-        if (scanf("%d", &n) == 1 && n > 0) {
-            break;
+    do {
+        int n;
+
+        // Введення кількості елементів масиву з перевіркою
+        while (1) {
+            printf("Введіть кількість елементів масиву: ");
+            if (scanf("%d", &n) == 1 && n > 0) {
+                break;
+            } else {
+                printf("Некоректне значення. Будь ласка, введіть позитивне ціле число.\n");
+                while (getchar() != '\n');  // Очищення буфера
+            }
+        }
+
+        int arr[n];  // Масив, заповнений випадковими числами
+
+        srand(time(NULL));  // Ініціалізація генератора випадкових чисел
+        for (int i = 0; i < n; i++) {
+            arr[i] = rand() % 201 - 100;  // Генерація випадкових чисел від -100 до 100
+        }
+
+        // Вивід згенерованого масиву
+        printf("Згенерований масив: ");
+        for (int i = 0; i < n; i++) {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+
+        // Знаходимо максимальний за модулем елемент
+        int max_abs = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            int abs_value = abs(arr[i]);
+            if (abs_value > max_abs) {
+                max_abs = abs_value;
+            }
+        }
+
+        // Знаходимо індекс останнього нульового елемента
+        int last_zero_index = -1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] == 0) {
+                last_zero_index = i;
+                break;
+            }
+        }
+
+        // Вивід результатів
+        printf("Максимальний за модулем елемент: %d\n", max_abs);
+
+        if (last_zero_index != -1) {
+            // Сума елементів після останнього нульового
+            int sum_after_last_zero = 0;
+            for (int i = last_zero_index + 1; i < n; i++) {
+                sum_after_last_zero += arr[i];
+            }
+            printf("Сума елементів після останнього нульового: %d\n", sum_after_last_zero);
         } else {
-            printf("Некоректне значення. Будь ласка, введіть позитивне ціле число.\n");
-            while (getchar() != '\n');  // Очищення буфера
+            printf("Немає жодного нульового значення в елементах масиву.\n");
         }
-    }
 
-    int arr[n];  // Масив, заповнений випадковими числами
-
-    srand(time(NULL));  // Ініціалізація генератора випадкових чисел
-    for (int i = 0; i < n; i++) {
-        arr[i] = rand() % 201 - 100;  // Генерація випадкових чисел від -100 до 100
-    }
-
-    // Знаходимо максимальний за модулем елемент
-    int max_abs = INT_MIN;
-    for (int i = 0; i < n; i++) {
-        int abs_value = abs(arr[i]);
-        if (abs_value > max_abs) {
-            max_abs = abs_value;
+        // Виведення меню
+        printf("Запустити ще раз? (1 - так, 0 - вийти): ");
+        while (1) {
+            if (scanf("%d", &choice) == 1 && (choice == 0 || choice == 1)) {
+                break;
+            } else {
+                printf("Некоректний вибір. Введіть 1 або 0: ");
+                while (getchar() != '\n');  // Очищення буфера
+            }
         }
-    }
-
-    // Знаходимо індекс останнього нульового елемента
-    int last_zero_index = -1;
-    for (int i = n - 1; i >= 0; i--) {
-        if (arr[i] == 0) {
-            last_zero_index = i;
-            break;
-        }
-    }
-
-    // Сума елементів після останнього нульового
-    int sum_after_last_zero = 0;
-    if (last_zero_index != -1) {
-        for (int i = last_zero_index + 1; i < n; i++) {
-            sum_after_last_zero += arr[i];
-        }
-    }
-
-    // Вивід результатів
-    printf("Максимальний за модулем елемент: %d\n", max_abs);
-    printf("Сума елементів після останнього нульового: %d\n", sum_after_last_zero);
+    } while (choice != 0);  // Продовжуємо виконання, поки користувач не вибере 0
 
     return 0;
 }
